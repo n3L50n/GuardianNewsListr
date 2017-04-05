d6a9540e36ed23a006ebfebf493702a96778cbf6
n0de_coyote <nelsonandrewwells@gmail.com>
2017-04-05 14:00:49 -0700

    Build out Adapter.

--- app/src/main/java/com/adventurekit/node_coyote/guardiannewslistr/ArticleAdapter.java
+++ app/src/main/java/com/adventurekit/node_coyote/guardiannewslistr/ArticleAdapter.java
@@ -1,17 +1,66 @@
 package com.adventurekit.node_coyote.guardiannewslistr;
 
 import android.content.Context;
-import android.support.annotation.LayoutRes;
 import android.support.annotation.NonNull;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
 
 /**
  * Created by node_coyote on 4/5/17.
  */
 
 public class ArticleAdapter extends ArrayAdapter<NewsArticle> {
-    
-    public ArticleAdapter(@NonNull Context context, @LayoutRes int resource) {
-        super(context, resource);
+
+    /**
+     * Constructor for building a new NewsArticle UI
+     * @param context  Given context
+     * @param articles A list of NewsArticles to fill a ListView
+     */
+    public ArticleAdapter(@NonNull Context context, ArrayList<NewsArticle> articles) {
+        super(context, 0 , articles);
+    }
+
+    /**
+     *
+     * @param position the position of the item being selected
+     * @param listItemCard a card view
+     * @param parent the parent
+     * @return a card view packed with a News Article
+     */
+    @Override
+    public View getView(int position, View listItemCard, ViewGroup parent){
+
+        // If there are no News Article cards, add them.
+        if (listItemCard == null){
+            listItemCard = LayoutInflater.from(getContext()).inflate(R.layout.card_view_frag, parent, false);
+        }
+
+        final NewsArticle currentArticle = getItem(position);
+
+        String articleTitle = currentArticle.getArticleTitle();
+        TextView titleTextView = (TextView) listItemCard.findViewById(R.id.news_article_title);
+        if (articleTitle != null && articleTitle.isEmpty()){
+            titleTextView.setText(articleTitle);
+        }
+
+        String sectionName = currentArticle.getArticleSectionName();
+        TextView sectionNameTextView = (TextView) listItemCard.findViewById(R.id.news_article_section_name);
+        if (sectionName != null && !sectionName.isEmpty()){
+            sectionNameTextView.setText(sectionName);
+        }
+
+        String pubDate = currentArticle.getDatePublished();
+        TextView pubDateTextView = (TextView) listItemCard.findViewById(R.id.news_article_date_published);
+        if (pubDate != null && !pubDate.isEmpty()){
+            pubDateTextView.setText(pubDate);
+        }
+
+        return listItemCard;
     }
 }
